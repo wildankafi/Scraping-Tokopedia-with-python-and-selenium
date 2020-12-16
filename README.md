b93e8e0c3989f8d6e2374b29d027b5ea7e869240
Wildan Mukafi <wildankafie@yahoo.co.id>
2020-12-17 05:59:17 +0700

    update reame

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Scraping-Tokopedia-with-python-and-selenium
+
+1. Create Folder results in project
+2. Chek version webdriver
+3. change the function according to url requirement (run_url())
--- run.py
+++ run.py
@@ -43,16 +43,7 @@ def get_url():
     if pages:
         lastpages = pages.get_text()
     totalpages = int(lastpages.replace('.', ''))
-    # product = soup.find_all(attrs={'class': 'css-bk6tzz e1nlzfl3'})
-    # Get product title
-    # for title in product:
-    #     print(title.find('a').find(attrs={'class':'css-1bjwylw'}).get_text())
-    # end Get product title
-    # urls= []
-    # for link in product:
-    #     url = link.find('a')['href']
-    #     urls.append(url)
-    for p in range(1,3):
+    for p in range(1,totalpages):
         soupPages = run_url(f"https://www.tokopedia.com/p/komputer-laptop/media-penyimpanan-data/harddisk-external?page={p}")
         product = soupPages.find_all(attrs={'class': 'css-bk6tzz e1nlzfl3'})
         # urls = []
@@ -72,71 +63,6 @@ def get_url():
         json.dump(data, outfile)
     # End Write file json
     print('Get URL link....')
-
-
-# def collect_product():
-#     with open('data.json') as json_file:
-#         data = json.load(json_file)
-#         for p in data['product']:
-#             detail = run_url(p['url'])
-#             dir = os.path.dirname(__file__)
-#             chrome_driver_path = dir + "\msedgedriver.exe"
-#             # create a new Chrome session
-#             driver = webdriver.Edge(chrome_driver_path)
-#             driver.implicitly_wait(30)
-#             driver.maximize_window()
-#             driver.get(p['url'])
-#             delay = 15  # seconds
-#             driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-#             try:
-#                 WebDriverWait(driver, delay).until(
-#                     EC.presence_of_element_located((By.CLASS_NAME, 'css-1mswclh')))
-#                 print("Page is ready!")
-#             except TimeoutException:
-#                 print(
-#                     "Loading took too much time!")
-#
-#             data = driver.page_source
-#             driver.quit()
-#             soup = BeautifulSoup(data, "html.parser")
-#             title = soup.find(attrs={'data-testid': 'lblPDPDetailProductName'}).get_text()
-#             rating = soup.find(attrs={'data-testid': 'lblPDPDetailProductRatingNumber'}).get_text()
-#             review = soup.find(attrs={'data-testid': 'lblPDPDetailProductRatingCounter'}).get_text()
-#             sale = soup.find(attrs={'data-testid': 'lblPDPDetailProductSuccessRate'}).get_text()
-#             view = soup.find(attrs={'data-testid': 'lblPDPDetailProductSeenCounter'}).get_text().split("x", 1)[0]
-#             price = soup.find(attrs={'data-testid': 'lblPDPDetailProductPrice'}).get_text().replace('Rp', '')
-#             weight = soup.find(attrs={'data-testid': 'PDPDetailWeightValue'}).get_text()
-#             condition = soup.find(attrs={'data-testid': 'PDPDetailConditionValue'}).get_text()
-#             description = soup.find(attrs={'data-testid': 'lblPDPDeskripsiProduk'}).get_text()
-#             store = soup.find(attrs={'data-testid': 'llbPDPFooterShopName'}).get_text()
-#             location = soup.find(attrs={'data-testid': 'lblPDPFooterLastOnline'}).get_text().split("\xa0•\xa0", 1)[0]
-#
-#
-#             dict_data = {
-#                 'title' :title,
-#                 'rating': rating,
-#                 'review':review,
-#                 'sale' : sale,
-#                 'view' : view,
-#                 'price' : price,
-#                 'weight' : weight,
-#                 'condition':condition,
-#                 'description':description,
-#                 'store' : store,
-#                 'location': location
-#             }
-#     #         print(p['url'])
-#
-#
-#     # print(dict_data)
-#     # print(description)
-#     # print(weight)
-#     # f = open('./res.html', 'w+')
-#     # f.write(rsc.text)
-#     # f.close()
-#     print(title.mystring.replace(" ", "_"))
-#
-#     print('Collect product ...')
 
 def detail_product(url):
     print('Get Detail Product', url)
@@ -197,7 +123,6 @@ def detail_product(url):
             no_punct = no_punct + char
     file = no_punct #title.replace(" ", "-").replace("[","").replace("]","").replace("","")
     with open('./results/{}.json'.format(file), 'w') as outfile:
-    # with open('./results/{}.json'.format(file), 'w') as outfile:
         json.dump(dict_data, outfile)
 
 def create_csv():
@@ -213,13 +138,11 @@ def create_csv():
     print('Generate CSV')
 
 def run():
-    # get_url()
-    # collect_product()
-    # detail_product("https://www.tokopedia.com/trinityplaza/seagate-backup-plus-slim-2tb-hdd-hd-hardisk-harddisk-external-hitam?whid=0")
-    # with open('data.json') as json_file:
-    #     data = json.load(json_file)
-    #     for p in data['product']:
-    #         detail_product(p['url'])
+    get_url()
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+        for p in data['product']:
+            detail_product(p['url'])
     create_csv()
 
 if __name__ == '__main__':
